d117ceaa56951f1ca9549108c7d80bc4cf3d2f67
Austin Hinkel <93750358+ahinkel@users.noreply.github.com>
2024-08-03 05:42:04 -0600

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -89,9 +89,9 @@ function calculate() {
 
     // Display the result
     document.getElementById('result1').textContent = `Estimated Annual Savings: $${annualSavingsOut.toFixed(2)}`;
-    document.getElementById('result2').textContent = `Estimated Cost of Array: $${arrayCostOut.toFixed(0)}`;
-    document.getElementById('result3').textContent = `Estimated Credit: $${creditAmountOut.toFixed(0)}`;
-    document.getElementById('result4').textContent = `Estimated Net Cost: $${netArrayCostOut.toFixed(0)}`;
+    document.getElementById('result2').textContent = `Estimated Cost of Array: $${arrayCostOut.toFixed(2)}`;
+    document.getElementById('result3').textContent = `Estimated Credit: $${creditAmountOut.toFixed(2)}`;
+    document.getElementById('result4').textContent = `Estimated Net Cost: $${netArrayCostOut.toFixed(2)}`;
     document.getElementById('result5').textContent = `Estimated Return Rate: ${returnRateOut.toFixed(3)}%`;
 }
 
